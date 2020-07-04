921c2e1aebdd1012761e55724a3fea7962dcab55
sherlockconnan <sherlockconnan@gmail.com>
2020-07-04 15:19:29 +0700

    Add project files.

---
+++ Infrastructure/Entities/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+            IsDeleted = false;
+            Description = "";
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public StatusEnum Status { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
+
---
+++ Infrastructure/Enums/GenderEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Enums
+{
+    public enum GenderEnum
+    {
+        Male = 1,
+        Female = 0,
+        Undefined = 2
+    }
+}
